ca8d0bf2ba85c7d945ae4cab28d105370856b1b6
Jandrews <jordandrews@hotmail.com>
2017-08-20 18:08:22 +0200

    Tests working - need to put duration in

--- test/splitter.js
+++ test/splitter.js
@@ -4,13 +4,13 @@ contract('Splitter', function(accounts) {
   it("should split amount between 2 accounts", function() {
     const amountToSplit = 1000;
     const increaseAmount = 500;
-    var initialBalance1 = web3.eth.getBalance(accounts[1]);
-    var initialBalance2 = web3.eth.getBalance(accounts[2]);
+    var initialBalance1 = web3.eth.getBalance(accounts[1]).toNumber();
+    var initialBalance2 = web3.eth.getBalance(accounts[2]).toNumber();
     return Splitter.deployed().then(function(instance) {
       return instance.sendTransaction({from: accounts[0], value: amountToSplit});
     }).then(function(tx) {
-      assert.equal(web3.eth.getBalance(accounts[1]), initialBalance1 + increaseAmount, "Account didn't increase by " + increaseAmount);
-      assert.equal(web3.eth.getBalance(accounts[2]), initialBalance2 + increaseAmount, "Account didn't increase by " + increaseAmount);
+      assert.equal(web3.eth.getBalance(accounts[1]).toNumber(), initialBalance1 + increaseAmount, "Account didn't increase by " + increaseAmount);
+      assert.equal(web3.eth.getBalance(accounts[2]).toNumber(), initialBalance2 + increaseAmount, "Account didn't increase by " + increaseAmount);
     });
   });
 });
