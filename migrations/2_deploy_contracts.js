8d3f4d9f981ceb089cf49282d9cb3737e1aaa933
Jandrews <jordandrews@hotmail.com>
2017-08-21 17:37:48 +0200

    Should work across accounts

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
 var Splitter = artifacts.require("./Splitter.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(Splitter, ["0x0887a6d264eda2c3c821b68cade7bf63586f54d1", "0x40d56af3c2ad6b15a31c5b2e59a3f8e35b26e8fb"]);
+  deployer.deploy(Splitter, [web3.eth.accounts[1], web3.eth.accounts[2]]);
 };
