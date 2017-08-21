ab4a723d1d82fe3c9e902c065fb111f39ea9f4ca
Jandrews <jordandrews@hotmail.com>
2017-08-21 10:40:45 +0200

    Need to test odd number splitting and joining of other users

--- contracts/Splitter.sol
+++ contracts/Splitter.sol
@@ -2,32 +2,41 @@ pragma solidity ^0.4.6;
 
 contract Splitter {
     address public sender;
-    address[] public receivers; // could add more users
+    address[] public receivers;
+    mapping(address => bool) receiverSearch;
     bool active;
 
     function Splitter(address[] ethReceivers){
         sender = msg.sender;
         receivers = ethReceivers;
+        for(uint i = 0; i < ethReceivers.length; i++){
+            receiverSearch[ethReceivers[i]] = true;
+        }
         active = true;
     }
 
-    function getContractBalance() constant returns (uint) {
+    function join(){
         if (!active) throw;
-        return this.balance;
-    }
-
-    function() payable {
-        if (!active) throw;
-        if (msg.sender == sender){
-            for(uint i =0; i < receivers.length; i++){
-                receivers[i].send(msg.value/receivers.length);
-            }
-        }
+        if (msg.sender == sender) throw; //optional - whether to allow owner
+        if (receiverSearch[msg.sender]) throw; //don't add same twice
+        receiverSearch[msg.sender] = true;
+        receivers.push(msg.sender);
     }
 
     function kill(){
         if (!active) throw;
         if (msg.sender != sender) throw;
         active = false;
+    }
+
+    // use default function so it works with only sendTransaction
+    function() payable {
+        if (!active) throw;
+        if (msg.sender == sender){
+            for(uint i =0; i < receivers.length; i++){
+                receivers[i].send(msg.value/receivers.length);
+            }
+            sender.send(msg.value % receivers.length);
+        }
     }
 }
--- test/splitter.js
+++ test/splitter.js
@@ -1,16 +1,22 @@
 var Splitter = artifacts.require('./Splitter.sol');
 
 contract('Splitter', function(accounts) {
-  it("should split amount between 2 accounts", function() {
-    const amountToSplit = 1000;
-    const increaseAmount = 500;
-    var initialBalance1 = web3.eth.getBalance(accounts[1]).toNumber();
-    var initialBalance2 = web3.eth.getBalance(accounts[2]).toNumber();
+  const alice = accounts[0];
+  const bob = accounts[1];
+  const carol = accounts[2];
+
+  it("should split amount evenly between 2 accounts", function() {
+    const toSplit = 1000;
+    const afterSplit = 500;
+    var bobBalance = web3.eth.getBalance(bob).toNumber();
+    var carolBalance = web3.eth.getBalance(carol).toNumber();
     return Splitter.deployed().then(function(instance) {
-      return instance.sendTransaction({from: accounts[0], value: amountToSplit});
+      return instance.sendTransaction({from: alice, value: toSplit});
     }).then(function(tx) {
-      assert.equal(web3.eth.getBalance(accounts[1]).toNumber(), initialBalance1 + increaseAmount, "Account didn't increase by " + increaseAmount);
-      assert.equal(web3.eth.getBalance(accounts[2]).toNumber(), initialBalance2 + increaseAmount, "Account didn't increase by " + increaseAmount);
+      assert.equal(web3.eth.getBalance(bob).toNumber(),
+      bobBalance + afterSplit, "Balance didn't increase by " + afterSplit);
+      assert.equal(web3.eth.getBalance(carol).toNumber(),
+      carolBalance + afterSplit, "Balance didn't increase by " + afterSplit);
     });
   });
 });
