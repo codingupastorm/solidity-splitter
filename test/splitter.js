a6b5a26ab419cd6da6cdb55383b26f15e5b6088a
Jandrews <jordandrews@hotmail.com>
2017-09-01 16:38:55 -0500

    Can't get test to work

--- test/splitter.js
+++ test/splitter.js
@@ -1,6 +1,8 @@
+web3.eth.getTransactionReceiptMined = require("../test/getTransactionReceiptMined.js");
 var Splitter = artifacts.require('./Splitter.sol');
 const Promise = require('bluebird');
 Promise.promisifyAll(web3.eth, {suffix: "Promise"});
+
 
 // Note that these tests rely on bob and carol not being miners.
 
@@ -14,58 +16,88 @@ contract('Splitter', function(accounts) {
     return Splitter.new({from: alice}).then(_splitter => splitter = _splitter);
   });
 
-  it("should save ether between two accounts", function() {
-    const toSplit = 1000;
-    const afterSplit = 500;
-    return splitter.split(bob, carol, {
-      from: alice,
-      value: toSplit
-    }).then(result => splitter.balances.call(bob)).then(bobBalance => assert.equal(bobBalance, afterSplit)).then(() => splitter.balances.call(carol)).then(carolBalance => assert.equal(carolBalance, afterSplit));
-  });
-
-  it("should split odd amount correctly", function() {
-    const toSplit = 3;
-    const afterSplit = 1;
-    return splitter.split(bob, carol, {
-      from: alice,
-      value: toSplit
-    }).then(result => splitter.balances.call(bob)).then(bobBalance => assert.equal(bobBalance, afterSplit)).then(() => splitter.balances.call(carol)).then(carolBalance => assert.equal(carolBalance, afterSplit)).then(() => splitter.balances.call(alice)).then(aliceBalance => assert.equal(aliceBalance, 1));
-  });
-
-  it("should withdraw correctly", function() {
-    const toSplit = 1000;
-    const afterSplit = 500;
-    const gasPrice = 10;
-    var bobStartBalance,
-      cost;
-    return web3.eth.getBalancePromise(bob).then((bobBalance) => {
-      bobStartBalance = bobBalance.toNumber();
-      return splitter.split(bob, carol, {
-        from: alice,
-        value: toSplit
-      });
-    }).then(result => splitter.claim({from: bob, gasPrice: gasPrice})).then(tx => {
-      cost = gasPrice * tx.receipt.gasUsed;
-      return web3.eth.getBalancePromise(bob);
-    }).then(bobFinalBalance => {
-      assert.equal(bobStartBalance + afterSplit - cost, bobFinalBalance.toNumber());
-    });
-  });
+  // it("should save ether between two accounts", function() {
+  //   const toSplit = 1000;
+  //   const afterSplit = 500;
+  //   return splitter.split(bob, carol, {
+  //     from: alice,
+  //     value: toSplit
+  //   }).then(result => splitter.balances.call(bob)).then(bobBalance => assert.equal(bobBalance, afterSplit)).then(() => splitter.balances.call(carol)).then(carolBalance => assert.equal(carolBalance, afterSplit));
+  // });
+  //
+  // it("should split odd amount correctly", function() {
+  //   const toSplit = 3;
+  //   const afterSplit = 1;
+  //   return splitter.split(bob, carol, {
+  //     from: alice,
+  //     value: toSplit
+  //   }).then(result => splitter.balances.call(bob)).then(bobBalance => assert.equal(bobBalance, afterSplit)).then(() => splitter.balances.call(carol)).then(carolBalance => assert.equal(carolBalance, afterSplit)).then(() => splitter.balances.call(alice)).then(aliceBalance => assert.equal(aliceBalance, 1));
+  // });
+  //
+  // it("should withdraw correctly", function() {
+  //   const toSplit = 1000;
+  //   const afterSplit = 500;
+  //   const gasPrice = 10;
+  //   var bobStartBalance,
+  //     cost;
+  //   return web3.eth.getBalancePromise(bob).then((bobBalance) => {
+  //     bobStartBalance = bobBalance.toNumber();
+  //     return splitter.split(bob, carol, {
+  //       from: alice,
+  //       value: toSplit
+  //     });
+  //   }).then(result => splitter.claim({from: bob, gasPrice: gasPrice})).then(tx => {
+  //     cost = gasPrice * tx.receipt.gasUsed;
+  //     return web3.eth.getBalancePromise(bob);
+  //   }).then(bobFinalBalance => {
+  //     assert.equal(bobStartBalance + afterSplit - cost, bobFinalBalance.toNumber());
+  //   });
+  // });
 
   it('should fail on amount too small', function() {
     const toSplit = 1;
-    return splitter.split(bob, carol, {
-      from: alice,
-      value: toSplit
-    }).then(result => assert.fail("Should have thrown error.")).catch(() => assert(true)); //Should also check it was not out of gas error
+    return expectedExceptionPromise(function(){
+      return splitter.split(bob, carol, {
+        from: alice,
+        value: toSplit
+      }, 30000000000);
+    });//.then(result => assert.fail("Should have thrown error.")).catch(() => assert(true)); //Should also check it was not out of gas error
   });
 
-  it("should fail on empty address", function() {
-    const toSplit = 1000;
-    return splitter.split(bob, "0x0", {
-      from: alice,
-      value: toSplit
-    }).then(result => assert.fail("Should have thrown error.")).catch(() => assert(true)); // Again, need to check for out of gas error
-  });
+  // it("should fail on empty address", function() {
+  //   const toSplit = 1000;
+  //   return splitter.split(bob, "0x0", {
+  //     from: alice,
+  //     value: toSplit
+  //   }).then(result => assert.fail("Should have thrown error.")).catch(() => assert(true)); // Again, need to check for out of gas error
+  // });
 
 });
+
+
+var expectedExceptionPromise = function (action, gasToUse) {
+  return new Promise(function (resolve, reject) {
+      try {
+        resolve(action());
+      } catch(e) {
+        reject(e);
+      }
+    })
+    .then(function (txn) {
+      // https://gist.github.com/xavierlepretre/88682e871f4ad07be4534ae560692ee6
+      return web3.eth.getTransactionReceiptMined(txn);
+    })
+    .then(function (receipt) {
+      // We are in Geth
+      assert.equal(receipt.gasUsed, gasToUse, "should have used all the gas");
+    })
+    .catch(function (e) {
+      if ((e + "").indexOf("invalid JUMP") > -1 || (e + "").indexOf("out of gas") > -1) {
+        // We are in TestRPC
+      } else if ((e + "").indexOf("please check your gas amount") > -1) {
+        // We are in Geth for a deployment
+      } else {
+        throw e;
+      }
+    });
+};
